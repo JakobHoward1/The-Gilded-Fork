145270836bdcf77e2f0a9cfbb9c21d113eafb44e
JakobHoward1 <jakob.howard@edu.sait.ca>
2025-08-20 15:15:35 -0600

    Initial commit

---
+++ App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace Group10_FinalProject
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new MainPage()) { Title = "Group10_FinalProject" };
+        }
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Group10_FinalProject
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
